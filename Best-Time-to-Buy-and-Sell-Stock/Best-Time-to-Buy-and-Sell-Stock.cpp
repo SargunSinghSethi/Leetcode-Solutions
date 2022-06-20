6dae3139f80cdf348d39c52d9a1630c12aadd4dd
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2022-06-20 15:32:38 +0530

    Create Best-Time-to-Buy-and-Sell-Stock.cpp

---
+++ Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+      int min = 100000;
+      int profit = 0;
+      
+      for(int i = 0; i < prices.size(); i++)
+      {
+        if(min > prices[i])
+        {
+          min = prices[i];
+        }
+        else if(profit < prices[i] - min)
+        {
+          profit = prices[i] - min;
+        }
+      }
+      return profit
+    }
+};
